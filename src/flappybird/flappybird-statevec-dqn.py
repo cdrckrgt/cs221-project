8257f48b71686ea2cb99a8d10e6aa134e05cef88
Cedrick Argueta <cdrckrgt@stanford.edu>
2018-11-16 12:24:49 -0800

    cleaning

--- src/flappybird/flappybird-statevec-dqn.py
+++ src/flappybird/flappybird-statevec-dqn.py
@@ -1,15 +1,15 @@
 import random
 import numpy as np
-import tensorflow as tf
 
+np.random.seed(4)
 random.seed(4)
+
+import tensorflow as tf
 tf.set_random_seed(4)
 
 # importing and creating the FlappyBird game
 from ple.games.flappybird import FlappyBird
 game = FlappyBird()
-# game.allowed_fps = None
-# game.scale = 10
 
 # to get nonvisual representations of the game, we need a state preprocessor
 def state_preprocessor(d):
@@ -105,25 +105,25 @@ model.add(Dense(nb_actions))
 model.add(Activation('linear'))
 print(model.summary())
 
-from rl.policy import EpsGreedyQPolicy,BoltzmannQPolicy
-
+from rl.policy import EpsGreedyQPolicy, BoltzmannQPolicy
 
 processor = None
 memory = SequentialMemory(limit=25000, window_length=1)
-dqn = DQNAgent(model=model,policy = EpsGreedyQPolicy(.1), nb_actions=nb_actions, memory=memory, processor=processor, nb_steps_warmup=10, gamma=.99, target_model_update=0.02)
+policy = EpsGreedyQPolicy(0.1)
+dqn = DQNAgent(model=model,policy=policy, nb_actions=nb_actions, memory=memory, processor=processor, nb_steps_warmup=100, gamma=.99, target_model_update=1e-2)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 p.display_screen = True
 
 from keras.callbacks import TensorBoard
-from keras.callbacks import ModelCheckpoint
+from rl.callbacks import ModelIntervalCheckpoint
 from time import time
 t = time()
 tb = TensorBoard(log_dir='../../logs/flappybird/{}'.format(t))
 
 
 filepath='../../weights/flappybird/best_{}.hdf5'.format(t)
-cp = ModelCheckpoint(filepath, verbose=1, period=5000)
+cp = ModelIntervalCheckpoint(filepath, verbose=1, period=5000)
 dqn.fit(env, nb_steps=30000, visualize=False, verbose=2, callbacks = [tb, cp])
 
 p.display_screen = True
