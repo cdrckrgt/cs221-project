e43b7119c48aa7ad8463674dead8de922f74a03b
Cristian Lomeli <cristianlomeli1999@gmail.com>
2018-11-30 20:57:12 -0800

    added linearlyannealed

--- src/flappybird/flappybird-statevec-dqn.py
+++ src/flappybird/flappybird-statevec-dqn.py
@@ -6,7 +6,6 @@ random.seed(4)
 
 import tensorflow as tf
 tf.set_random_seed(4)
-np.random.seed(4)
 
 # importing and creating the FlappyBird game
 from ple.games.flappybird import FlappyBird
@@ -106,16 +105,88 @@ model.add(Dense(nb_actions))
 model.add(Activation('linear'))
 print(model.summary())
 
-from rl.policy import EpsGreedyQPolicy, BoltzmannQPolicy
+from rl.util import *
+
+# class Policy(object):
+#     """Abstract base class for all implemented policies.
+#     Each policy helps with selection of action to take on an environment.
+#     Do not use this abstract base class directly but instead use one of the concrete policies implemented.
+#     To implement your own policy, you have to implement the following methods:
+#     - `select_action`
+#     # Arguments
+#         agent (rl.core.Agent): Agent used
+#     """
+#     def _set_agent(self, agent):
+#         self.agent = agent
+#
+#     @property
+#     def metrics_names(self):
+#         return []
+#
+#     @property
+#     def metrics(self):
+#         return []
+#
+#     def select_action(self, **kwargs):
+#         raise NotImplementedError()
+#
+#     def get_config(self):
+#         """Return configuration of the policy
+#         # Returns
+#             Configuration as dict
+#         """
+#         return {}
+#
+#
+# class EpsGreedyQPolicy(Policy):
+#     """Implement the epsilon greedy policy
+#     Eps Greedy policy either:
+#     - takes a random action with probability epsilon
+#     - takes current best action with prob (1 - epsilon)
+#     """
+#     def __init__(self, eps=.4, decay = .99995, mineps = .1):
+#         super(EpsGreedyQPolicy, self).__init__()
+#         self.eps = eps
+#         self.decay = decay
+#         self.mineps = mineps
+#
+#     def select_action(self, q_values):
+#         """Return the selected action
+#         # Arguments
+#             q_values (np.ndarray): List of the estimations of Q for each action
+#         # Returns
+#             Selection action
+#         """
+#         assert q_values.ndim == 1
+#         nb_actions = q_values.shape[0]
+#
+#         if np.random.uniform() < self.eps:
+#             action = np.random.random_integers(0, nb_actions-1)
+#         else:
+#             action = np.argmax(q_values)
+#         if self.eps > self.mineps:
+#             self.eps *= self.decay
+#         return action
+#
+#     def get_config(self):
+#         """Return configurations of EpsGreedyQPolicy
+#         # Returns
+#             Dict of config
+#         """
+#         config = super(EpsGreedyQPolicy, self).get_config()
+#         config['eps'] = self.eps
+#         return config
+
+
+from rl.policy import MaxBoltzmannQPolicy, LinearAnnealedPolicy, EpsGreedyQPolicy
 
 processor = None
-memory = SequentialMemory(limit=25000, window_length=1)
-<<<<<<< HEAD
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, processor=processor, nb_steps_warmup=10, gamma=.99, target_model_update=0.01)
-=======
-policy = EpsGreedyQPolicy(0.1)
-dqn = DQNAgent(model=model,policy=policy, nb_actions=nb_actions, memory=memory, processor=processor, nb_steps_warmup=100, gamma=.99, target_model_update=1e-2)
->>>>>>> 8257f48b71686ea2cb99a8d10e6aa134e05cef88
+memory = SequentialMemory(limit=75000, window_length=1)
+# policy = EpsGreedyQPolicy()
+# policy = MaxBoltzmannQPolicy()
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=.2, value_min = .05, value_test = .02,
+                            nb_steps = 30000)
+dqn = DQNAgent(model=model,policy=policy, nb_actions=nb_actions, memory=memory, processor=processor, nb_steps_warmup=50, gamma=.99, target_model_update=1e-2)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 p.display_screen = True
@@ -127,8 +198,8 @@ t = time()
 tb = TensorBoard(log_dir='../../logs/flappybird/{}'.format(t))
 filepath='../../weights/flappybird/best_{}.hdf5'.format(t)
 cp = ModelIntervalCheckpoint(filepath, verbose=1, interval=5000)
-dqn.fit(env, nb_steps=30000, visualize=False, verbose=2, callbacks = [tb, cp])
+dqn.fit(env, nb_steps=75000, visualize=False, verbose=2, callbacks = [tb, cp])
 
 p.display_screen = True
 
-dqn.test(env, nb_episodes=5, visualize=True)
+dqn.test(env, nb_episodes=15, visualize=True)
