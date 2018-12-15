7f39a8cfe1cc87e43de1b3ab3e1940554b7518b9
Austin Chow <archow@stanford.edu>
2018-12-14 20:00:25 -0800

    steps changed

--- src/pixelcopter/pixelcopter-statevec.py
+++ src/pixelcopter/pixelcopter-statevec.py
@@ -195,7 +195,7 @@ t = time()
 tb = TensorBoard(log_dir='../../logs/pixelcopter/{}'.format(t))
 filepath='../../weights/pixelcopter/best_{}.hdf5'.format(t)
 cp = ModelIntervalCheckpoint(filepath, verbose=1, interval=5000)
-dqn.fit(env, nb_steps=150000, visualize=False, verbose=1, callbacks = [tb,cp])
+dqn.fit(env, nb_steps=75000, visualize=False, verbose=1, callbacks = [tb,cp])
 
 p.display_screen = True
 
